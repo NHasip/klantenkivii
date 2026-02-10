e482218cce21b8215e6311e7789e1deec364e73f
NHasip <neciphasip@gmail.com>
2026-02-10 15:40:14 +0100

    Use plain POST when form-section action is set

--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -7,8 +7,12 @@
     </x-section-title>
 
     <div class="mt-5 md:mt-0 md:col-span-2">
-        <form wire:submit.prevent="{{ $submit }}" method="POST" @if($action) action="{{ $action }}" @endif>
-            @csrf
+        @if($action)
+            <form method="POST" action="{{ $action }}">
+                @csrf
+        @else
+            <form wire:submit.prevent="{{ $submit }}">
+        @endif
             <div class="px-4 py-5 bg-white sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
                 <div class="grid grid-cols-6 gap-6">
                     {{ $form }}
@@ -20,6 +24,6 @@
                     {{ $actions }}
                 </div>
             @endif
-        </form>
+            </form>
     </div>
 </div>
